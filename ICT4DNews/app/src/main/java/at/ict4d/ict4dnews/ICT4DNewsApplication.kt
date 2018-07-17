2108a5b242da2ef5b25c6e114ea77aced0868762
rajasone <rajasone@gmail.com>
2018-07-18 02:52:05 +0500

    Fixes #9 - Add LeakCanary

--- ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/ICT4DNewsApplication.kt
+++ ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/ICT4DNewsApplication.kt
@@ -5,6 +5,7 @@ import at.ict4d.ict4dnews.dagger.components.ApplicationComponent
 import at.ict4d.ict4dnews.dagger.components.DaggerApplicationComponent
 import at.ict4d.ict4dnews.dagger.modules.ApplicationModule
 import com.facebook.stetho.Stetho
+import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 class ICT4DNewsApplication : Application() {
@@ -16,11 +17,17 @@ class ICT4DNewsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
         initDagger()
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
-            Stetho.initializeWithDefaults(this);
+            Stetho.initializeWithDefaults(this)
         }
     }
 
