017a50e5c21fe515c4f332ca5f2b3a63f4677d27
rajasone <rajasone@gmail.com>
2018-07-23 20:41:24 +0500

    Refs #13 - Dagger Improvements

--- ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/dagger/components/ApplicationComponent.kt
+++ ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/dagger/components/ApplicationComponent.kt
@@ -4,11 +4,12 @@ import at.ict4d.ict4dnews.ICT4DNewsApplication
 import at.ict4d.ict4dnews.dagger.modules.*
 import dagger.Component
 import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [ApplicationModule::class, RoomModule::class, ApiServiceModule::class, HelperModule::class, ViewModelFactoryModule::class])
-interface ApplicationComponent: AndroidInjector<ICT4DNewsApplication> {
+@Component(modules = [AndroidSupportInjectionModule::class, ApplicationModule::class, RoomModule::class, ApiServiceModule::class, HelperModule::class, ViewModelFactoryModule::class])
+interface ApplicationComponent : AndroidInjector<ICT4DNewsApplication> {
 
     @Component.Builder
     abstract class Builder : AndroidInjector.Builder<ICT4DNewsApplication>()
--- ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/dagger/modules/ApplicationModule.kt
+++ ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/dagger/modules/ApplicationModule.kt
@@ -1,26 +1,16 @@
 package at.ict4d.ict4dnews.dagger.modules
 
-import android.app.Application
-import at.ict4d.ict4dnews.ICT4DNewsApplication
 import at.ict4d.ict4dnews.dagger.modules.activities.ICT4DNewsDetailActivityModule
 import at.ict4d.ict4dnews.dagger.modules.activities.MainNavigationActivityModule
 import at.ict4d.ict4dnews.dagger.scopes.PerActivity
 import at.ict4d.ict4dnews.screens.MainNavigationActivity
 import at.ict4d.ict4dnews.screens.news.detail.ICT4DNewsDetailActivity
-import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.android.ContributesAndroidInjector
-import dagger.android.support.AndroidSupportInjectionModule
-import javax.inject.Singleton
 
 @Suppress("unused")
-@Module(includes = [AndroidSupportInjectionModule::class])
+@Module
 abstract class ApplicationModule {
-
-    @Binds
-    @Singleton
-    abstract fun application(application: ICT4DNewsApplication): Application
 
     @PerActivity
     @ContributesAndroidInjector(modules = [ICT4DNewsDetailActivityModule::class])
