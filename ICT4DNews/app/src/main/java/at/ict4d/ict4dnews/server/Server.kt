05ec7c84f4b36841dd02eeca2c2b37ef2aa07750
Paul Spiesberger <paul.spiesberger@rise-world.com>
2018-07-27 23:02:29 +0200

    refs #10 - adds code style formatter settings to project

--- ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/server/Server.kt
+++ ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/server/Server.kt
@@ -21,83 +21,94 @@ class Server @Inject constructor(
 
     override fun loadICT4DatRSSFeed(): Disposable {
         return apiRSSService.getRssICT4DatNews()
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .subscribe(
-                        { channel ->
-                            Timber.d("*** $channel")
-                        },
-                        { error: Throwable? ->
-                            Timber.e(error)
-                        })
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe(
+                { channel ->
+                    Timber.d("*** $channel")
+                },
+                { error: Throwable? ->
+                    Timber.e(error)
+                })
     }
 
     override fun loadICT4DatJsonFeed(): Disposable {
 
         return apiJsonSelfHostedWPService.getJsonICT4DatNews()
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .subscribe({ serverPosts: List<SelfHostedWPPost> ->
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe({ serverPosts: List<SelfHostedWPPost> ->
 
-                    // Query for the authors
-                    val serverAuthors: MutableList<WordpressAuthor> = mutableListOf()
-                    for (post in serverPosts.distinctBy { it.serverAuthorID }) {
-                        val author = apiJsonSelfHostedWPService.getJsonICT4DatAuthorByID(serverAuthorID = post.serverAuthorID).execute().body()
+                // Query for the authors
+                val serverAuthors: MutableList<WordpressAuthor> = mutableListOf()
+                for (post in serverPosts.distinctBy { it.serverAuthorID }) {
+                    val author =
+                        apiJsonSelfHostedWPService.getJsonICT4DatAuthorByID(serverAuthorID = post.serverAuthorID)
+                            .execute().body()
 
-                        author?.let {
-                            serverAuthors.add(author)
-                        }
+                    author?.let {
+                        serverAuthors.add(author)
                     }
+                }
 
-                    // Query for all media elements per each post
-                    val serverMedia: MutableList<WordpressMedia> = mutableListOf()
-                    for (post in serverPosts) {
-                        val postMedia = apiJsonSelfHostedWPService.getJsonICT4DatMediaForPost(post.serverID).execute().body()
+                // Query for all media elements per each post
+                val serverMedia: MutableList<WordpressMedia> = mutableListOf()
+                for (post in serverPosts) {
+                    val postMedia =
+                        apiJsonSelfHostedWPService.getJsonICT4DatMediaForPost(post.serverID).execute().body()
 
-                        if (postMedia != null) {
-                            serverMedia += postMedia
-                        }
+                    if (postMedia != null) {
+                        serverMedia += postMedia
                     }
+                }
 
-                    // Set up foreign keys for media
-                    serverMedia.map { m -> m.postLink = serverPosts.find { post -> post.serverID == m.serverPostID }?.link ?: "" }
-                    serverMedia.map { m -> m.authorLink = serverAuthors.find { author -> author.server_id == m.serverAuthorID }?.link ?: "" }
+                // Set up foreign keys for media
+                serverMedia.map { m ->
+                    m.postLink = serverPosts.find { post -> post.serverID == m.serverPostID }?.link ?: ""
+                }
+                serverMedia.map { m ->
+                    m.authorLink = serverAuthors.find { author -> author.server_id == m.serverAuthorID }?.link ?: ""
+                }
 
-                    // Set up foreign key for posts
-                    serverPosts.map { post -> post.authorLink = serverAuthors.find { author -> author.server_id == post.serverAuthorID }?.link ?: "" }
-                    serverPosts.map { post -> post.featuredMediaLink = serverMedia.find { media -> media.serverPostID == post.serverID }?.linkRaw ?: "" }
+                // Set up foreign key for posts
+                serverPosts.map { post ->
+                    post.authorLink = serverAuthors.find { author -> author.server_id == post.serverAuthorID }?.link ?: ""
+                }
+                serverPosts.map { post ->
+                    post.featuredMediaLink = serverMedia.find { media -> media.serverPostID == post.serverID }?.linkRaw ?: ""
+                }
 
-                    // Map to local models
-                    val authors = serverAuthors.map { AuthorModel(it) }
-                    val news = serverPosts.map { NewsModel(it) }
-                    val media = serverMedia.map { MediaModel(it) }
+                // Map to local models
+                val authors = serverAuthors.map { AuthorModel(it) }
+                val news = serverPosts.map { NewsModel(it) }
+                val media = serverMedia.map { MediaModel(it) }
 
-                    // Strip HTML
-                    news.map { n ->
-                        n.title?.let {
-                            n.title = it.stripHtml()
-                        }
-                        n.description?.let {
-                            n.description = it.stripHtml()
-                        }
+                // Strip HTML
+                news.map { n ->
+                    n.title?.let {
+                        n.title = it.stripHtml()
                     }
-
-                    media.map { m ->
-                        m.description?.let {
-                            m.description = it.stripHtml()
-                        }
+                    n.description?.let {
+                        n.description = it.stripHtml()
                     }
+                }
 
-                    Timber.d("$serverPosts")
-                    Timber.d("$serverAuthors")
-                    Timber.d("$serverMedia")
+                media.map { m ->
+                    m.description?.let {
+                        m.description = it.stripHtml()
+                    }
+                }
 
-                    persistenceManager.insertAllAuthors(authors)
-                    persistenceManager.insertAllNews(news)
-                    persistenceManager.insertAllMedia(media)
-                }, {
-                    Timber.e("Error in ICT4D.at Call")
-                    Timber.e(it)
-                })
+                Timber.d("$serverPosts")
+                Timber.d("$serverAuthors")
+                Timber.d("$serverMedia")
+
+                persistenceManager.insertAllAuthors(authors)
+                persistenceManager.insertAllNews(news)
+                persistenceManager.insertAllMedia(media)
+            }, {
+                Timber.e("Error in ICT4D.at Call")
+                Timber.e(it)
+            })
     }
 }
