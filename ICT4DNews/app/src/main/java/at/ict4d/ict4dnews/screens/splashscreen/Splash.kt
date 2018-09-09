0e7d07a38e6fb78ddd7436afe6b34eaa717db5e6
Paul Spiesberger <paul.spiesberger@ict4d.at>
2018-09-09 20:58:03 +0200

    refs #30 and refs #46 - fixes hardcoded Strings, improves Splash Screen

--- ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/screens/splashscreen/Splash.kt
+++ ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/screens/splashscreen/Splash.kt
@@ -1,33 +1,33 @@
 package at.ict4d.ict4dnews.screens.splashscreen
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.app.AppCompatActivity
 import at.ict4d.ict4dnews.R
 import at.ict4d.ict4dnews.screens.MainNavigationActivity
 
-const val SPLASH_DELAY: Long = 3000
+const val SPLASH_DELAY: Long = 1500
 
 class Splash : AppCompatActivity() {
-    private var delayHandler = Handler()
-    internal val runnable: Runnable = Runnable {
+    private val delayHandler = Handler()
+
+    private val runnable: Runnable = Runnable {
         if (!isFinishing) {
             val intent = Intent(applicationContext, MainNavigationActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        delayHandler = Handler()
         delayHandler.postDelayed(runnable, SPLASH_DELAY)
     }
+
     public override fun onDestroy() {
-        if (true) {
-            delayHandler.removeCallbacks(runnable)
-        }
+        delayHandler.removeCallbacks(runnable)
         super.onDestroy()
     }
 }
