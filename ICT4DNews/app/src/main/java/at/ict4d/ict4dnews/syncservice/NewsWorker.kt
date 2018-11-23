d380451f568796b03deebe7a65620c58f26d0eee
rajasone <rajasone@gmail.com>
2018-11-23 19:43:52 +0500

    Refs #121 - Introduced CountDown Latch For NewsWorker

--- ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/syncservice/NewsWorker.kt
+++ ICT4DNews/app/src/main/java/at/ict4d/ict4dnews/syncservice/NewsWorker.kt
@@ -7,8 +7,10 @@ import at.ict4d.ict4dnews.ICT4DNewsApplication
 import at.ict4d.ict4dnews.server.IServer
 import at.ict4d.ict4dnews.utils.NewsRefreshDoneMessage
 import at.ict4d.ict4dnews.utils.RxEventBus
+import at.ict4d.ict4dnews.utils.ServerErrorMessage
 import io.reactivex.disposables.CompositeDisposable
 import timber.log.Timber
+import java.util.concurrent.CountDownLatch
 import javax.inject.Inject
 
 class NewsWorker(context: Context, workParams: WorkerParameters) : Worker(context, workParams) {
@@ -21,18 +23,38 @@ class NewsWorker(context: Context, workParams: WorkerParameters) : Worker(contex
 
     private val compositeDisposable = CompositeDisposable()
 
+    private var isNewsLoadedSuccessfully: Boolean = false
+
+    private val countDownLatch: CountDownLatch = CountDownLatch(1)
+
+    // TODO("Remove all of these log statements")
+
     init {
         ICT4DNewsApplication.instance.component.inject(this)
+
         compositeDisposable.add(rxEventBus.filteredObservable(NewsRefreshDoneMessage::class.java).subscribe {
-            Timber.d("News updated successfully")
+            isNewsLoadedSuccessfully = true
+            Timber.d("News updated successfully latch count is ----> ${countDownLatch.count}")
+            countDownLatch.countDown()
+        })
+
+        compositeDisposable.add(rxEventBus.filteredObservable(ServerErrorMessage::class.java).subscribe {
+            isNewsLoadedSuccessfully = false
+            Timber.d("Failed to update news")
+            countDownLatch.countDown()
         })
     }
 
     override fun doWork(): Result {
-        Timber.d("Start the doWork")
-        server.loadAllNewsFromAllActiveBlogs()
-        Timber.d("End the doWork")
+        Timber.d("Start the doWork status of news loading ----> $isNewsLoadedSuccessfully")
 
-        return Result.SUCCESS
+        server.loadAllNewsFromAllActiveBlogs()
+        if (countDownLatch.count > 0) {
+            Timber.d("Going to call await for this thread")
+            countDownLatch.await()
+        }
+        Timber.d("End the doWork status of news loading ----> $isNewsLoadedSuccessfully --- count down of latch is ---> ${countDownLatch.count}")
+
+        return if (isNewsLoadedSuccessfully) Result.SUCCESS else Result.RETRY
     }
 }
