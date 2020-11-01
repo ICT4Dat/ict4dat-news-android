d62fb020dc3f2bd3ccf9cd7d54fb24b9f7d01bf6
Paul Spiesberger <paul@spiesberger.eu>
2020-11-01 10:13:29 +0100

    Removes Dead Code and changes URL of Paul

--- ICT4DNews/app/src/main/kotlin/at/ict4d/ict4dnews/extensions/ICT4DExtensions.kt
+++ ICT4DNews/app/src/main/kotlin/at/ict4d/ict4dnews/extensions/ICT4DExtensions.kt
@@ -128,31 +128,6 @@ fun String.stripHtml(): String = Jsoup.parse(this).text()
 
 fun RecyclerView.moveToTop(moveToPosition: Int = 0) = smoothScrollToPosition(moveToPosition)
 
-/**
- * LiveData that propagates only distinct emissions.
- * @see https://medium.com/google-developers/7-pro-tips-for-room-fbadea4bfbd1
- */
-fun <T> LiveData<T>.getDistinct(): LiveData<T> {
-    val distinctLiveData = MediatorLiveData<T>()
-    distinctLiveData.addSource(this, object : Observer<T> {
-        private var initialized = false
-        private var lastObj: T? = null
-        override fun onChanged(obj: T?) {
-            if (!initialized) {
-                initialized = true
-                lastObj = obj
-                distinctLiveData.postValue(lastObj)
-            } else if ((obj == null && lastObj != null) ||
-                obj != lastObj
-            ) {
-                lastObj = obj
-                distinctLiveData.postValue(lastObj)
-            }
-        }
-    })
-    return distinctLiveData
-}
-
 fun NavController.navigateSafe(
     @IdRes currentDestination: Int,
     action: NavDirections? = null,
