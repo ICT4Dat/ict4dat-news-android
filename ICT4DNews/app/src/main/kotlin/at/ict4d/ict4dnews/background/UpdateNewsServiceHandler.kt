caa82145cbee8d228bfbdf6cfc11565bac1e43f9
Paul Spiesberger <paul@spiesberger.eu>
2020-11-01 12:23:34 +0100

    Adds Backoff Criteria to News Update Worker

--- ICT4DNews/app/src/main/kotlin/at/ict4d/ict4dnews/background/UpdateNewsServiceHandler.kt
+++ ICT4DNews/app/src/main/kotlin/at/ict4d/ict4dnews/background/UpdateNewsServiceHandler.kt
@@ -1,10 +1,10 @@
 package at.ict4d.ict4dnews.background
 
 import android.os.Build
+import androidx.work.BackoffPolicy
 import androidx.work.Constraints
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.NetworkType
-import androidx.work.Operation
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import java.util.concurrent.TimeUnit
@@ -14,27 +14,30 @@ class UpdateNewsServiceHandler(private val workManager: WorkManager) {
     fun requestToRegisterService() {
         val newsWork = PeriodicWorkRequestBuilder<UpdateNewsWorker>(1, TimeUnit.DAYS)
             .setConstraints(getConstraintsForNewsSyncService())
+            .setBackoffCriteria(
+                BackoffPolicy.LINEAR,
+                1, // retry after 1 hour
+                TimeUnit.HOURS
+            )
             .addTag(NEWS_WORKER_TAG)
             .build()
         workManager.enqueueUniquePeriodicWork(NEWS_WORKER_TAG, ExistingPeriodicWorkPolicy.KEEP, newsWork)
     }
 
-    fun cancelTask(): Operation {
-        return workManager.cancelAllWorkByTag(NEWS_WORKER_TAG)
-    }
+    fun cancelTask() = workManager.cancelAllWorkByTag(NEWS_WORKER_TAG)
 
-    private fun getConstraintsForNewsSyncService(): Constraints {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .setRequiresBatteryNotLow(true)
-                .setRequiresDeviceIdle(true)
-                .build()
-        } else {
-            Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.CONNECTED)
-                .setRequiresBatteryNotLow(true)
-                .build()
-        }
+    private fun getConstraintsForNewsSyncService() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresDeviceIdle(true)
+            .setRequiresStorageNotLow(true)
+            .build()
+    } else {
+        Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresStorageNotLow(true)
+            .build()
     }
 }
