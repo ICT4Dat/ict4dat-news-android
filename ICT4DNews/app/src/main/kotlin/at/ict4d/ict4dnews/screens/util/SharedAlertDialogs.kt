741cb78c380801028ca334eb1d3cfdc8a8f09eb7
Paul Spiesberger <paul@spiesberger.eu>
2020-12-27 20:57:53 +0100

    Code and text improvements
    fixes #215

--- ICT4DNews/app/src/main/kotlin/at/ict4d/ict4dnews/screens/util/SharedAlertDialogs.kt
+++ ICT4DNews/app/src/main/kotlin/at/ict4d/ict4dnews/screens/util/SharedAlertDialogs.kt
@@ -5,13 +5,12 @@ import at.ict4d.ict4dnews.R
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 fun showOwnershipAlertDialog(context: Context, okClicked: () -> Unit = {}) {
-    val dialog = MaterialAlertDialogBuilder(context)
+    MaterialAlertDialogBuilder(context)
         .setTitle(R.string.ownership_dialog_title)
         .setMessage(R.string.ownership_dialog_message)
         .setPositiveButton(R.string.dialog_ok) { _, _ ->
             okClicked.invoke()
         }
         .setCancelable(false)
-
-    dialog.show()
+        .show()
 }
